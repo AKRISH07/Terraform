e14d47459dd518a4e2b978b55c366b6c136a7f45
AKRISH07 <98654625+AKRISH07@users.noreply.github.com>
2022-04-10 15:50:03 +0530

    Jenkins

---
+++ Jenkins.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region="us-east-1"
+}
+
+resource "aws_s3_bucket" "BucketCreation" {
+  bucket = "anands3bucket2022"
+  tags = {
+    "Name" = "BucketFromTerraform"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "BucketVersioning"{
+    bucket = aws_s3_bucket.BucketCreation.id
+    versioning_configuration {
+      status="Enabled"
+    }
+}
+
+resource "aws_kms_key" "KeyForS3" {
+  description = "KMS_Key"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "SSEKMS_Encryption" {
+  bucket = aws_s3_bucket.BucketCreation.id
+  rule {
+    apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.KeyForS3.arn
+        sse_algorithm= "aws:kms"
+        }
+    }
+}
+
+resource "aws_s3_bucket_website_configuration" "Static_Website_Hosting"{
+bucket=aws_s3_bucket.BucketCreation.id
+index_document{
+    suffix = "Index.html"
+    }
+}
